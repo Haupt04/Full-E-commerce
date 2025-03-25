4644a6272ee234d97162c1fb4643eb6e9193726b
Haupt04 <116957227+Haupt04@users.noreply.github.com>
2025-03-25 16:52:40 +0200

    Initial Commit and Server Setup

---
+++ backend/server.js
@@ -0,0 +1,18 @@
+import express from "express"
+import dotenv from "dotenv"
+import authRoutes from "./routes/auth.route.js"
+import { connectDB } from "./lib/db.js"
+
+
+dotenv.config() // Allow us to access the environment variable 
+
+const app = express()
+const PORT = process.env.PORT || 5000;
+
+app.use("/api/auth", authRoutes)
+
+
+app.listen(PORT, () => {
+    connectDB(); //Connecting Database 
+    console.log("Server is running on port:", PORT)
+})
