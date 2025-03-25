7522e35fa507d24e98351ca350b12e92bc2f7ce1
Haupt04 <116957227+Haupt04@users.noreply.github.com>
2025-03-25 16:53:46 +0200

    Create User Routes anc Controllers

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,24 @@
+export const signup = async (req, res) => {
+    try {
+
+    } catch (error) {
+
+    }
+}
+
+export const login = async (req, res) => {
+    try {
+
+    } catch (error) {
+        
+    }
+}
+
+
+export const logout = async (req, res) => {
+    try {
+
+    } catch (error) {
+        
+    }
+}
