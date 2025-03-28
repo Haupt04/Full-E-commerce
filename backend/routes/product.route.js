0feffbe3f26f04717a04971540fd431a92b14429
Haupt04 <116957227+Haupt04@users.noreply.github.com>
2025-03-28 09:34:36 +0200

    Completed Product Routes and Controllers

--- backend/routes/product.route.js
+++ backend/routes/product.route.js
@@ -1,11 +1,18 @@
 import express from "express"
-import { getAllProducts } from "../controllers/product.controller.js"
+import { createProduct, deleteProduct, getAllProducts, getFeaturedProducts, getProductsByCategory, getRecommendedProducts, toggleFeaturedProduct } from "../controllers/product.controller.js"
 import { adminRoute, protectRoute } from "../middleware/auth.middleware.js"
 
 
 const router = express.Router()
 
 router.get("/",protectRoute, adminRoute, getAllProducts)
+router.get("/featured", getFeaturedProducts)
+router.get("/recommendations", getRecommendedProducts)
+router.get("/category/:category", getProductsByCategory)
+router.patch("/:id", protectRoute, adminRoute, toggleFeaturedProduct)
+router.post("/", protectRoute, adminRoute, createProduct);
+router.delete("/:id", protectRoute, adminRoute, deleteProduct)
+
 
 
 export default router
