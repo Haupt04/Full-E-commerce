0e3707aa6d9a2f46b8c52fe00a0e18b421adc198
Haupt04 <116957227+Haupt04@users.noreply.github.com>
2025-04-09 10:01:43 +0200

    Completed changes for deployment

--- backend/server.js
+++ backend/server.js
@@ -8,12 +8,17 @@ import cartRoutes from "./routes/cart.route.js"
 import analyticsRoutes from "./routes/analytic.route.js"
 import couponRoutes from "./routes/coupon.route.js"
 import paymentRoutes from "./routes/payment.route.js"
+import path from "path"
 
 
 dotenv.config() // Allow us to access the environment variable 
 
 const app = express()
 const PORT = process.env.PORT || 5000;
+
+const __dirname = path.resolve();
+
+
 
 app.use(express.json({limit: '10mb'})) // ready parse data
 app.use(cookieParser())
@@ -25,6 +30,13 @@ app.use("/api/coupons", couponRoutes)
 app.use("/api/payments", paymentRoutes)
 app.use("/api/analytics", analyticsRoutes)
 
+if (process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+    app.get("*", (req,res) => {
+        res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
+    })
+}
 
 
 app.listen(PORT, () => {
